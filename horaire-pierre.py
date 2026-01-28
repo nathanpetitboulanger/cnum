ebe119a1bde3f7bf24875e858e195324f2405e71
Pierre Tauzin <tauzin_pierre@orange.fr>
2026-01-28 16:17:21 +0100

    Ceci est le première version du début des apprentissages - pierre

---
+++ horaire-pierre.py
@@ -0,0 +1,33 @@
+# Tests api google sheet
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# 1. Définir le périmètre (Scope)
+# On définit ici que l'on veut accéder aux feuilles et au drive
+
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
+
+# 2. Authentification avec ton fichier JSON
+# Remplace "nom_de_ta_cle.json" par le vrai nom de ton fichier
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "token.json",
+    scope,  # type: ignore
+)
+
+client = gspread.authorize(creds)  # type: ignore
+
+
+# 3. Ouverture du document
+# Tu peux ouvrir par le titre exact ou par l'ID (présent dans l'URL)
+sheet_name = "API"
+spreadsheet = client.open(sheet_name)
+sheet = spreadsheet.sheet1  # Accède au premier onglet
+
+
+
+# début du scraping 
+values = sheet.get_all_values()
+print(values)
