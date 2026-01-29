c57f3cd3a9f4c855981162c540085d83504718f1
Nathan <nath39.np@gmail.com>
2026-01-29 14:44:33 +0100

    ajout fonction recuperation delta time

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,4 +1,6 @@
 from gspread import spreadsheet
+from datetime import datetime
+import dateparser
 
 
 def get_cell_color(spreadsheet, sheet_numbrer: int, row: int, col: int):
@@ -81,3 +83,43 @@ def get_text_from_merged_cell(data, merge):
     raw = merge["startRowIndex"]
     col = merge["startColumnIndex"]
     return data[raw][col]
+
+
+def get_time_delta_from_merge(data, merge):
+    id_col_time = 0
+    time_format = "%H:%M"
+
+    start_row_id = merge["startRowIndex"]
+    end_row_id = merge["endRowIndex"]
+    start_col_id = merge["startColumnIndex"]
+
+    ## Récupération id dates
+
+    row = start_row_id
+    col = id_col_time
+
+    actual_time_position = [row, col]
+    actual_time = "date"
+    while actual_time != "":
+        actual_time_position[0] = actual_time_position[0] - 1
+        row = actual_time_position[0]
+        col = actual_time_position[1]
+        actual_time = data[row][col]
+
+    raw_start_str = data[start_row_id][id_col_time]
+    raw_end_str = data[end_row_id][id_col_time]
+
+    time_start = [time.strip() for time in raw_start_str.split("\n")][0]
+    time_end = [time.strip() for time in raw_end_str.split("\n")][1]
+
+    start_time_object = datetime.strptime(time_start, time_format).time()
+    end_time_object = datetime.strptime(time_end, time_format).time()
+
+    date_str = data[start_row_id - 1][start_col_id]
+
+    date_obj = dateparser.parse(date_str).date()  # type: ignore
+
+    start_datetime = datetime.combine(date_obj, start_time_object)
+    end_datetime = datetime.combine(date_obj, end_time_object)
+
+    return (start_datetime, end_datetime)
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 # Tests api google sheet
+import dateparser
 import gspread
+from datetime import datetime
 from oauth2client.service_account import ServiceAccountCredentials
 from src.utils.functions import *
 from src.utils.dummies import *
@@ -20,11 +22,16 @@ spreadsheet = client.open(sheet_name)
 
 sheet = spreadsheet.get_worksheet(1)
 data = sheet.get_all_values()
-
 all_merges = get_all_merges(sheet)
-
-
 merge = all_merges[3]
 
-for merge in all_merges:
-    print(get_text_from_merged_cell(data, merge))
+data
+merge = {
+    "sheetId": 739511890,
+    "startRowIndex": 6,
+    "endRowIndex": 10,
+    "startColumnIndex": 3,
+}
+
+
+delta = get_time_delta_from_merge(data, merge)
